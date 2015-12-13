896437946248ba31cbb9286eef2b23871a55f8cd
Karin Touchie <ktouchie@gmail.com>
2015-12-13 18:34:52 +0100

    Create variables and loop outline with error messages for bad input

--- Activite4.java
+++ Activite4.java
@@ -11,7 +11,32 @@ class Activite4 {
         System.out.println("1 - Convert Celsius to Fahrenheit");
         System.out.println("2 - Convert Fahrenheit to Celsius");
         
+        int mode = sc.nextInt();
         
+        char loop = 'Y';
         
+        while (loop == 'Y') {
+        
+            if (mode==1) {
+                //* Celsius to Fahrenheit *//
+                System.out.println("Please enter the temperature (°C) you wish to convert:");
+                
+            } else if (mode==2) {
+                //* Fahreheit to Celsius *//
+            } else {
+                System.out.println("That is not a valid selection. Please select /"1/" or /"2/".");
+            }
+            System.out.println("Would you like to convert another temperature? (Y/N)");
+            char valid = sc.next().charAt(0);
+            valid = Character.toUpperCase(valid);
+            if (valid=='Y') {
+                loop = 'Y';
+            } else if (valid=='N') {
+                loop = 'N';
+            } else {
+                System.out.println("That is not a valid selection.");
+                loop = 'N';
+            }
+        }
     }
 }
