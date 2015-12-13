7dc8b6c7b32a5c62e26dfdbcb71248a0669d746c
Karin Touchie <ktouchie@gmail.com>
2015-12-13 19:13:46 +0100

    Move converter title outside loop, compile

--- Activite4.java
+++ Activite4.java
@@ -10,10 +10,11 @@ class Activite4 {
         double cels = 0;
         double fahr = 0;
         
+        System.out.println("CELSIUS-FAHRENHEIT CONVERTER");
+        System.out.println("----------------------------");
+        
         while (loop == 'Y') {        
         
-            System.out.println("CELSIUS-FAHRENHEIT CONVERTER");
-            System.out.println("----------------------------");
             System.out.println("Choose conversion mode:");
             System.out.println("1 - Convert Celsius to Fahrenheit");
             System.out.println("2 - Convert Fahrenheit to Celsius");
