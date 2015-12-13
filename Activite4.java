6dead050fd9da3c9d5a5846a65fa4a3a89b4b48a
Karin Touchie <ktouchie@gmail.com>
2015-12-13 18:07:02 +0100

    Add Scanner and inital prompts

---
+++ Activite4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Activite4 {
+    
+    public static void main(String [] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("CELSIUS-FAHRENHEIT CONVERTER");
+        System.out.println("----------------------------");
+        System.out.println("Choose conversion mode:");
+        System.out.println("1 - Convert Celsius to Fahrenheit");
+        System.out.println("2 - Convert Fahrenheit to Celsius");
+        
+        
+        
+    }
+}
